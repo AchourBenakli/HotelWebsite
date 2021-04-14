4f10e786dc7444adf49d3fd6b01f1d5c95eaaac2
Achour Benakli <73812092+AchourBenakli@users.noreply.github.com>
2021-04-14 03:07:27 +0200

    first commit

---
+++ js/app1.js
@@ -0,0 +1,7 @@
+const description = document.getElementsByClassName('content');
+
+  for (i=0; i<hero-1-SpeechRecognitionAlternative.length; i++){
+    description[i].addEventListener('click', function(){
+      this.classList.toggle('active')
+    })
+  }
