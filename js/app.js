f38dffbc78ef945c0357d7f34f8dcc5ab6db6e9f
Achour Benakli <73812092+AchourBenakli@users.noreply.github.com>
2021-05-07 22:12:22 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,43 +1,9 @@
-// // const selectElement = function (element) {
-// //   return document.querySelector(element);
+let submit = document.getElementsByClassName('submit');
 
-// // };
-
-// // let menuToggler = selectElement('.menu-toggle');
-// // let body = selectElement('body');
-
-// // menuToggler.addEventListener('click', function () {
-
-// //   body.classList.toggle('open');
-
-// // });
-
-// // window.sr = ScrollReveal();
-
-// // sr.reveal('.animate-left', {
-// //   origin: 'left',
-// //   duration: 1000,
-// //   distance: '25ren',
-// //   delay: 300
-// // });
-// // sr.reveal('.animate-right', {
-// //   origin: 'right',
-// //   duration: 1000,
-// //   distance: '25ren',
-// //   delay: 600
-// // });
-// // sr.reveal('.animate-top', {
-// //   origin: 'top',
-// //   duration: 1000,
-// //   distance: '25ren',
-// //   delay: 600
-// // });
-// // sr.reveal('.animate-bottom', {
-// //   origin: 'bottom',
-// //   duration: 1000,
-// //   distance: '25ren',
-// //   delay: 600
-// });
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  alert('Hello world');
+})
 
 
 
--- js/app1.js
+++
@@ -1,7 +0,0 @@
-const description = document.getElementsByClassName('content');
-
-  for (i=0; i<hero-1-SpeechRecognitionAlternative.length; i++){
-    description[i].addEventListener('click', function(){
-      this.classList.toggle('active')
-    })
-  }
